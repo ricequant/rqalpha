23096039d8a594cefb41f624e5a7f094c56c7bea
Cedric Porter <cedricporter@gmail.com>
2016-07-18 11:13:31 +0800

    add MANIFEST.in

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ from pip.req import parse_requirements
 
 setup(
     name='rqbacktest',
-    version='0.0.3',
+    version='0.0.4',
     description='Python Distribution Utilities',
     packages=find_packages(exclude=[]),
     author='ricequant',
     author_email='public@ricequant.com',
     url='https://www.ricequant.com/',
     install_requires=[str(ir.req) for ir in parse_requirements("requirements.txt", session=False)],
-
+    zip_safe=False,
     entry_points={
         "console_scripts": [
             "rqbacktest = rqbacktest.__main__:entry_point",
