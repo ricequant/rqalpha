992d80fcb5e25b709534b57d20b936934b4433bf
Cedric Porter <cedricporter@gmail.com>
2016-07-18 11:27:45 +0800

    update version to 0.0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from pip.req import parse_requirements
 
 setup(
     name='rqbacktest',
-    version='0.0.4',
+    version='0.0.5',
     description='Python Distribution Utilities',
     packages=find_packages(exclude=[]),
     author='ricequant',
