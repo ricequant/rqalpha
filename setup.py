8617109261b97b0c08ae661f4e07ec7d354db4eb
Cedric Porter <cedricporter@gmail.com>
2016-07-18 10:54:31 +0800

    add entry point and change log

--- setup.py
+++ setup.py
@@ -7,11 +7,18 @@ from pip.req import parse_requirements
 
 setup(
     name='rqbacktest',
-    version='0.0.1',
+    version='0.0.2',
     description='Python Distribution Utilities',
     packages=find_packages(exclude=[]),
     author='ricequant',
     author_email='public@ricequant.com',
     url='https://www.ricequant.com/',
     install_requires=[str(ir.req) for ir in parse_requirements("requirements.txt", session=False)],
+
+    entry_points={
+        "console_scripts": [
+            "rqbacktest = rqbacktest.__main__:entry_point",
+        ]
+    },
+
 )
