7498fdc6e43f12568e14432392aac6613c9c16a1
Cedric Porter <cedricporter@gmail.com>
2016-07-20 18:42:51 +0800

    update version and url

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ from pip.req import parse_requirements
 
 setup(
     name='rqalpha',
-    version='0.0.12',
+    version='0.0.13',
     description='Ricequant Backtest Engine',
     packages=find_packages(exclude=[]),
     author='ricequant',
     author_email='public@ricequant.com',
-    url='https://www.ricequant.com/',
+    url='https://github.com/ricequant/rqalpha',
     install_requires=[str(ir.req) for ir in parse_requirements("requirements.txt", session=False)],
     zip_safe=False,
     entry_points={
