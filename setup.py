0ebe3d243f9ce3435091e2b9915dffbc187a8276
Cedric Porter <cedricporter@gmail.com>
2016-07-25 11:49:49 +0800

    update version to 0.0.17

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from pip.req import parse_requirements
 
 setup(
     name='rqalpha',
-    version='0.0.16',
+    version='0.0.17',
     description='Ricequant Backtest Engine',
     packages=find_packages(exclude=[]),
     author='ricequant',
