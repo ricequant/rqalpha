477830136c52892d70671f1c667672eea246fadc
Cedric Porter <cedricporter@gmail.com>
2016-07-27 10:50:15 +0800

    RQAlpha : Add scipy & statsmodels into RQAlpha

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from pip.req import parse_requirements
 
 setup(
     name='rqalpha',
-    version='0.0.56',
+    version='0.0.57',
     description='Ricequant Backtest Engine',
     packages=find_packages(exclude=[]),
     author='ricequant',
