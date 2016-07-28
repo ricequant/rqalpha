8a8dd0b3a8fede6db93a02ae2ce0192cb3414b7a
Cedric Porter <cedricporter@gmail.com>
2016-07-28 15:44:37 +0800

    remove scipy from requirements

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from pip.req import parse_requirements
 
 setup(
     name='rqalpha',
-    version='0.0.63',
+    version='0.0.64',
     description='Ricequant Backtest Engine',
     packages=find_packages(exclude=[]),
     author='ricequant',
