d32976d728112e39cd57200e466744401de0b4f0
EricWang <wangh@ricequant.com>
2017-02-10 14:16:50 +0800

    disable travis temporarily

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,6 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
