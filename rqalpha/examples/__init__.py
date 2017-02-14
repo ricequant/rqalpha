c45d86a776dc62cb8c173ef4a80011fcc405c6e8
EricWang <wangh@ricequant.com>
2017-02-14 14:59:26 +0800

    release v0.3.3 解决 `rqalpha examples -d .` 无样例策略生成的问题

---
+++ rqalpha/examples/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 Ricequant, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+RQAlpha - a Algorithm Trading System
+"""
+
+import pkgutil
+
+__all__ = [
+    '__version__',
+    'version_info'
+]
+
+__version__ = pkgutil.get_data(__package__, 'VERSION.txt').decode('ascii').strip()
+
+version_info = tuple(int(v) if v.isdigit() else v
+                     for v in __version__.split('.'))
+
+del pkgutil
