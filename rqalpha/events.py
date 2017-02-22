42e7c962261e182b6abafe1b6db6288a08efbc90
EricWang <wangh@ricequant.com>
2017-02-22 10:31:48 +0800

    add events comment

--- rqalpha/events.py
+++ rqalpha/events.py
@@ -19,24 +19,33 @@ from enum import Enum
 
 class Events(Enum):
     # 系统初始化后触发
+    # post_system_init()
     POST_SYSTEM_INIT = 'post_system_init'
     # 策略执行完init函数后触发
+    # post_user_init()
     POST_USER_INIT = 'post_user_init'
     # 策略证券池发生变化后触发
+    # post_universe_changed(universe)
     POST_UNIVERSE_CHANGED = 'post_universe_changed'
 
     # 执行before_trading函数前触发
+    # pre_before_trading()
     PRE_BEFORE_TRADING = 'pre_before_trading'
     # 该事件会触发策略的before_trading函数
+    # before_trading()
     BEFORE_TRADING = 'before_trading'
     # 执行before_trading函数后触发
+    # post_before_trading()
     POST_BEFORE_TRADING = 'post_before_trading'
 
     # 执行handle_bar函数前触发
+    # pre_bar()
     PRE_BAR = 'pre_bar'
     # 该事件会触发策略的handle_bar函数
+    # bar(bar_dict)
     BAR = 'bar'
     # 执行handle_bar函数后触发
+    # post_bar()
     POST_BAR = 'post_bar'
 
     # 预定义事件，支持handle_tick后使用
@@ -50,35 +59,49 @@ class Events(Enum):
     POST_SCHEDULED = 'post_scheduled'
 
     # 执行after_trading函数前触发
+    # pre_after_trading()
     PRE_AFTER_TRADING = 'pre_after_trading'
     # 该事件会触发策略的after_trading函数
+    # after_trading()
     AFTER_TRADING = 'after_trading'
     # 执行after_trading函数后触发
+    # post_after_trading()
     POST_AFTER_TRADING = 'post_after_trading'
 
     # 结算前触发该事件
+    # pre_settlement()
     PRE_SETTLEMENT = 'pre_settlement'
     # 触发结算事件
+    # settlement()
     SETTLEMENT = 'settlement'
     # 结算后触发该事件
+    # post_settlement()
     POST_SETTLEMENT = 'post_settlement'
 
     # 创建订单
+    # order_pending_new(account, order)
     ORDER_PENDING_NEW = 'order_pending_new'
     # 创建订单成功
+    # order_creation_pass(account, order)
     ORDER_CREATION_PASS = 'order_creation_pass'
     # 创建订单失败
+    # order_creation_reject(account, order)
     ORDER_CREATION_REJECT = 'order_creation_reject'
     # 创建撤单
+    # order_pending_cancel(account, order)
     ORDER_PENDING_CANCEL = 'order_pending_cancel'
     # 撤销订单成功
+    # order_cancellation_pass(account, order)
     ORDER_CANCELLATION_PASS = 'order_cancellation_pass'
     # 撤销订单失败
+    # order_cancellation_reject(account, order)
     ORDER_CANCELLATION_REJECT = 'order_cancellation_reject'
     # 订单状态更新
+    # order_unsolicited_update(account, order)
     ORDER_UNSOLICITED_UPDATE = 'order_unsolicited_update'
 
     # 成交
+    # trade(accout, trade)
     TRADE = 'trade'
 
     ON_LINE_PROFILER_RESULT = 'on_line_profiler_result'
