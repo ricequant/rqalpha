5a7973f611d1044535c6236d5524a527111ffab6
EricWang <wangh@ricequant.com>
2017-03-10 11:45:25 +0800

    fix update_bundle regression bug

--- rqalpha/__init__.py
+++ rqalpha/__init__.py
@@ -42,6 +42,6 @@ def run(config, source_code=None):
     return main.run(parse_config(config, click_type=False, source_code=source_code), source_code=source_code)
 
 
-def update_bundle(data_bundle_path=None, confirm=True):
+def update_bundle(data_bundle_path=None, locale="zh_Hans_CN", confirm=True):
     from . import main
-    main.update_bundle(data_bundle_path, confirm)
+    main.update_bundle(data_bundle_path=data_bundle_path, locale=locale, confirm=confirm)
