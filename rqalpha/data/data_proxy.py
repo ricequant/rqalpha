5d9e72b75a6e82bfdb0814e708858c846879d20f
Hongzhang Liu <hongzhang.liu@gmail.com>
2017-04-03 20:14:10 +0800

    fix

--- rqalpha/data/data_proxy.py
+++ rqalpha/data/data_proxy.py
@@ -172,14 +172,14 @@ class DataProxy(InstrumentMixin, TradingDatesMixin):
 
     def is_suspended(self, order_book_id, dt, count=1):
         if count == 1:
-            return self._data_source.is_suspended(order_book_id, [dt])[0]
+            return self._data_source.is_suspended(order_book_id, [dt])
 
         trading_dates = self.get_n_trading_dates_until(dt, count)
         return self._data_source.is_suspended(order_book_id, trading_dates)
 
     def is_st_stock(self, order_book_id, dt, count=1):
         if count == 1:
-            return self._data_source.is_st_stock(order_book_id, [dt])[0]
+            return self._data_source.is_st_stock(order_book_id, [dt])
 
         trading_dates = self.get_n_trading_dates_until(dt, count)
         return self._data_source.is_st_stock(order_book_id, trading_dates)
