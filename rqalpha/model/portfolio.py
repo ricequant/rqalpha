845cb8706874606925bda0d306affd8bf5b7ba39
Cedric Porter <cedricporter@gmail.com>
2017-04-20 19:11:59 +0800

    fix bug when running future

--- rqalpha/model/portfolio.py
+++ rqalpha/model/portfolio.py
@@ -144,7 +144,7 @@ class Portfolio(object):
         """
         [float] 累计年化收益率
         """
-        current_date = Environment.get_instance().calendar_dt.date()
+        current_date = Environment.get_instance().trading_dt.date()
         return self.unit_net_value ** (DAYS_CNT.DAYS_A_YEAR / float((current_date - self.start_date).days + 1)) - 1
 
     @property
