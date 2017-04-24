e882162e9ab7364a441f5c07ab8ba1da5995652b
Cedric Porter <cedricporter@gmail.com>
2017-04-24 15:03:21 +0800

    fix history_bars

--- rqalpha/mod/rqalpha_mod_sys_simulation/simulation_event_source.py
+++ rqalpha/mod/rqalpha_mod_sys_simulation/simulation_event_source.py
@@ -148,6 +148,7 @@ class SimulationEventSource(AbstractEventSource):
                 date = day.to_pydatetime()
                 last_tick = None
                 last_dt = None
+                dt_before_day_trading = date.replace(hour=8, minute=30)
                 while True:
                     for tick in data_proxy.get_merge_ticks(self._get_universe(), date, last_dt):
                         # find before trading time
@@ -159,7 +160,13 @@ class SimulationEventSource(AbstractEventSource):
                                         trading_dt=before_trading_dt)
 
                         dt = tick.datetime
-                        yield Event(EVENT.TICK, calendar_dt=dt, trading_dt=dt, tick=tick)
+
+                        if dt < dt_before_day_trading:
+                            trading_dt = dt.replace(year=date.year, month=date.month, day=date.day)
+                        else:
+                            trading_dt = dt
+
+                        yield Event(EVENT.TICK, calendar_dt=dt, trading_dt=trading_dt, tick=tick)
 
                         if self._universe_changed:
                             self._universe_changed = False
