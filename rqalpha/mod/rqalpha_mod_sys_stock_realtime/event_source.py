9d6d676462749929a003c0740e1a59ce19be2335
Onion <bsknight58@gmail.com>
2017-05-05 14:28:40 +0800

    1、修复python2兼容性问题；2、修复all_instruments接口修改后导致rqalpha_mod_sys_stock_realtime报错 (#123)
    * test
    
    * modify df index
    
    * commit
    
    * test
    
    * modify df index
    
    * commit
    
    * merge 2.0.6
    
    * commit
    
    * commit
    
    * fix no settlement
    
    * delete tags
    
    * revert files
    
    * commit
    
    * com it
    
    * add jqapi
    
    * fix bugs
    
    * fix bugs
    
    * commit
    
    * commit
    
    * Delete ii
    
    * Delete tags
    
    * commit
    
    * commit
    
    * Update event_source.py
    
    * Update event_source.py

--- rqalpha/mod/rqalpha_mod_sys_stock_realtime/event_source.py
+++ rqalpha/mod/rqalpha_mod_sys_stock_realtime/event_source.py
@@ -63,7 +63,9 @@ class RealtimeEventSource(AbstractEventSource):
     def quotation_worker(self):
         while True:
             if not is_holiday_today() and is_tradetime_now():
-                order_book_id_list = sorted(Environment.get_instance().data_proxy.all_instruments("CS").order_book_id.tolist())
+                current_date = datetime.datetime(self._env.trading_dt.year, self._env.trading_dt.month, self._env.trading_dt.day)
+                #order_book_id_list = sorted(Environment.get_instance().data_proxy.all_instruments("CS",current_date).order_book_id.tolist())
+                order_book_id_list = sorted([instruments.order_book_id for instruments in Environment.get_instance().data_proxy.all_instruments("CS", current_date)])
                 code_list = [order_book_id_2_tushare_code(code) for code in order_book_id_list]
 
                 try:
