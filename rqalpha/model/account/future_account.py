88935b9643fa07c574749c963f6da34e098df33d
EricWang <me@emptystack.net>
2017-05-05 18:12:48 +0800

    using prepend_listener for core module event register

--- rqalpha/model/account/future_account.py
+++ rqalpha/model/account/future_account.py
@@ -42,12 +42,12 @@ class FutureAccount(BaseAccount):
 
     def register_event(self):
         event_bus = Environment.get_instance().event_bus
-        event_bus.add_listener(EVENT.SETTLEMENT, self._settlement)
-        event_bus.add_listener(EVENT.ORDER_PENDING_NEW, self._on_order_pending_new)
-        event_bus.add_listener(EVENT.ORDER_CREATION_REJECT, self._on_order_creation_reject)
-        event_bus.add_listener(EVENT.ORDER_CANCELLATION_PASS, self._on_order_unsolicited_update)
-        event_bus.add_listener(EVENT.ORDER_UNSOLICITED_UPDATE, self._on_order_unsolicited_update)
-        event_bus.add_listener(EVENT.TRADE, self._on_trade)
+        event_bus.prepend_listener(EVENT.SETTLEMENT, self._settlement)
+        event_bus.prepend_listener(EVENT.ORDER_PENDING_NEW, self._on_order_pending_new)
+        event_bus.prepend_listener(EVENT.ORDER_CREATION_REJECT, self._on_order_creation_reject)
+        event_bus.prepend_listener(EVENT.ORDER_CANCELLATION_PASS, self._on_order_unsolicited_update)
+        event_bus.prepend_listener(EVENT.ORDER_UNSOLICITED_UPDATE, self._on_order_unsolicited_update)
+        event_bus.prepend_listener(EVENT.TRADE, self._on_trade)
 
     def fast_forward(self, orders, trades=list()):
         # 计算 Positions
