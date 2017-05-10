f1328748d1e6c0c6a5df6e027b33937dd4c8828f
Cedric Porter <cedricporter@gmail.com>
2017-05-10 14:23:50 +0800

    fix event

--- rqalpha/model/account/benchmark_account.py
+++ rqalpha/model/account/benchmark_account.py
@@ -30,7 +30,7 @@ class BenchmarkAccount(StockAccount):
         event_bus = Environment.get_instance().event_bus
         event_bus.add_listener(EVENT.SETTLEMENT, self._on_settlement)
         event_bus.add_listener(EVENT.PRE_BAR, self._on_bar)
-        event_bus.add_listener(EVENT.PRE_TICK, self._on_tick)
+        event_bus.add_listener(EVENT.TICK, self._on_tick)
 
     def _on_bar(self, event):
         # run once
