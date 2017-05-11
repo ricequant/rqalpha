70eb83a975a461778e94a8c96206e07f191b723d
EricWang <me@emptystack.net>
2017-05-11 20:36:41 +0800

    update default_dir_path

--- rqalpha/__main__.py
+++ rqalpha/__main__.py
@@ -24,6 +24,7 @@ from importlib import import_module
 
 from .utils.click_helper import Date
 from .utils.config import parse_config, get_mod_config_path, dump_config, load_mod_config
+from .utils.config import default_dir_path
 
 
 @click.group()
@@ -57,7 +58,7 @@ def entry_point():
 
 
 @cli.command()
-@click.option('-d', '--data-bundle-path', default=os.path.expanduser("~/.rqalpha"), type=click.Path(file_okay=False))
+@click.option('-d', '--data-bundle-path', default=default_dir_path, type=click.Path(file_okay=False))
 @click.option('--locale', 'locale', type=click.STRING, default="zh_Hans_CN")
 def update_bundle(data_bundle_path, locale):
     """
