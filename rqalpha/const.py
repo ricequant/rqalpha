3a9b5a46b9ecbfb8059d0d96dc9f7e0adade60ee
EricWang <me@emptystack.net>
2017-06-05 11:42:19 +0800

    update

--- rqalpha/const.py
+++ rqalpha/const.py
@@ -49,9 +49,9 @@ class RUN_TYPE(CustomEnum):
 # noinspection PyPep8Naming
 class ACCOUNT_TYPE(CustomEnum):
     """
-    关于 ACCOUNT_TYPE，目前主要表示为交易账户。STOCK / FUTURE / OPTION 目前均表示为中国 对应的交易账户。
-    ACCOUNT_TYPE 不区分交易所，比如 A 股区分上海交易所和深圳交易所，但对应的都是一个账户，因此统一为 STOCK
-    而 Bitcoin 对应很多不同的交易所，所以需要指定交易所来进行账户的区分
+    *   关于 ACCOUNT_TYPE，目前主要表示为交易账户。STOCK / FUTURE / OPTION 目前均表示为中国 对应的交易账户。
+    *   ACCOUNT_TYPE 不区分交易所，比如 A 股区分上海交易所和深圳交易所，但对应的都是一个账户，因此统一为 STOCK
+    *   目前暂时不添加其他 ACCOUNT_TYPE 类型，尤其是 CURRENCY, 外汇比较特别，其本质是cash, 应该和 position 对立, 未来可能提供一个更强大的 cash 对象，可以方便进行currency的计算、统计和换汇等内容。
     """
     TOTAL = 0
     BENCHMARK = 1
@@ -61,10 +61,6 @@ class ACCOUNT_TYPE(CustomEnum):
     FUTURE = 3
     # 期权
     OPTION = 4
-    # 货币
-    CURRENCY = 5
-    # 火币交易所
-    HUOBI_ALTCOIN = 6
 
 
 # noinspection PyPep8Naming
