98f9e892d4867b4067fad6235df0f984100e1561
EricWang <me@emptystack.net>
2017-06-15 11:22:39 +0800

    bug fixed

--- rqalpha/mod/rqalpha_mod_sys_simulation/utils.py
+++ rqalpha/mod/rqalpha_mod_sys_simulation/utils.py
@@ -26,10 +26,6 @@ def init_portfolio(env):
     start_date = config.base.start_date
     total_cash = 0
 
-    if ACCOUNT_TYPE.FUTURE.name in config.base.account_list and config.base.frequency != '1d':
-        StockAccount.AGGRESSIVE_UPDATE_LAST_PRICE = True
-        FutureAccount.AGGRESSIVE_UPDATE_LAST_PRICE = True
-
     for account_type in config.base.account_list:
         if account_type == DEFAULT_ACCOUNT_TYPE.STOCK.name:
             stock_starting_cash = config.base.stock_starting_cash
@@ -37,6 +33,8 @@ def init_portfolio(env):
                 raise RuntimeError(_(u"stock starting cash can not be 0, using `--stock-starting-cash 100000`"))
             StockAccount = env.get_account_model(DEFAULT_ACCOUNT_TYPE.STOCK.name)
             StockPosition = env.get_position_model(DEFAULT_ACCOUNT_TYPE.STOCK.name)
+            if DEFAULT_ACCOUNT_TYPE.FUTURE.name in config.base.account_list and config.base.frequency != '1d':
+                StockAccount.AGGRESSIVE_UPDATE_LAST_PRICE = True
             accounts[DEFAULT_ACCOUNT_TYPE.STOCK.name] = StockAccount(stock_starting_cash, Positions(StockPosition))
             total_cash += stock_starting_cash
         elif account_type == DEFAULT_ACCOUNT_TYPE.FUTURE.name:
@@ -45,6 +43,8 @@ def init_portfolio(env):
                 raise RuntimeError(_(u"future starting cash can not be 0, using `--future-starting-cash 100000`"))
             FutureAccount = env.get_account_model(DEFAULT_ACCOUNT_TYPE.FUTURE.name)
             FuturePosition = env.get_position_model(DEFAULT_ACCOUNT_TYPE.FUTURE.name)
+            if config.base.frequency != '1d':
+                FutureAccount.AGGRESSIVE_UPDATE_LAST_PRICE = True
             accounts[DEFAULT_ACCOUNT_TYPE.FUTURE.name] = FutureAccount(future_starting_cash, Positions(FuturePosition))
             total_cash += future_starting_cash
         else:
