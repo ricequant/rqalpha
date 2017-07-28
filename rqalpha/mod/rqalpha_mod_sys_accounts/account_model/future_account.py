c4d184f0aec40ceab3ad13b9b6d254b86a51ecfd
Hongzhang Liu <hongzhang.liu@gmail.com>
2017-07-28 14:30:13 +0800

    fix frozen cash

--- rqalpha/mod/rqalpha_mod_sys_accounts/account_model/future_account.py
+++ rqalpha/mod/rqalpha_mod_sys_accounts/account_model/future_account.py
@@ -168,7 +168,7 @@ class FutureAccount(BaseAccount):
     @staticmethod
     def _frozen_cash_of_order(order):
         if order.position_effect == POSITION_EFFECT.OPEN:
-            return margin_of(order.order_book_id, order.quantity, order.frozen_price)
+            return margin_of(order.order_book_id, order.unfilled_quantity, order.frozen_price)
         else:
             return 0
 
