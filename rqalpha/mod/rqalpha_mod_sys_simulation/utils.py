d29040c0ae00ecfa4c05820053e63e41cb4bf080
Rambo <dev.wenjunch@gmail.com>
2017-10-24 16:48:39 +0800

    add init_positions filter

--- rqalpha/mod/rqalpha_mod_sys_simulation/utils.py
+++ rqalpha/mod/rqalpha_mod_sys_simulation/utils.py
@@ -32,6 +32,15 @@ def _fake_trade(order_book_id, quantity, price):
                                  POSITION_EFFECT.OPEN, order_book_id)
 
 
+def _filter_positions(env, account_type):
+    positions = env.config.base.init_positions
+    futures = [ins.order_book_id for ins in env.data_proxy.all_instruments('Future')]
+    if account_type == 'FUTURE':
+        return list(filter(lambda p: p[0] in futures, positions))
+    else:
+        return list(filter(lambda p: p[0] not in futures, positions))
+
+
 def init_portfolio(env):
     accounts = {}
     config = env.config
@@ -50,27 +59,26 @@ def init_portfolio(env):
         position_model = env.get_position_model(account_type)
         positions = Positions(position_model)
 
-        if account_type in config.base.init_positions:
-            for order_book_id, quantity in config.base.init_positions[account_type]:
-                instrument = env.get_instrument(order_book_id)
-                if instrument is None:
-                    raise RuntimeError(_(u'invalid order book id {} in initial positions').format(order_book_id))
-                if not instrument.listing:
-                    raise RuntimeError(_(u'instrument {} in initial positions is not listing').format(order_book_id))
+        for order_book_id, quantity in _filter_positions(env, account_type):
+            instrument = env.get_instrument(order_book_id)
+            if instrument is None:
+                raise RuntimeError(_(u'invalid order book id {} in initial positions').format(order_book_id))
+            if not instrument.listing:
+                raise RuntimeError(_(u'instrument {} in initial positions is not listing').format(order_book_id))
 
-                bars = env.data_proxy.history_bars(order_book_id, 1, '1d', 'close',
-                                                   env.data_proxy.get_previous_trading_date(start_date),
-                                                   adjust_type='none')
-                if bars is None:
-                    raise RuntimeError(_(u'the close price of {} in initial positions is not available').format(order_book_id))
+            bars = env.data_proxy.history_bars(order_book_id, 1, '1d', 'close',
+                                               env.data_proxy.get_previous_trading_date(start_date),
+                                               adjust_type='none')
+            if bars is None:
+                raise RuntimeError(_(u'the close price of {} in initial positions is not available').format(order_book_id))
 
-                price = bars[0]
-                trade = _fake_trade(order_book_id, quantity, price)
-                if order_book_id not in positions:
-                    positions[order_book_id] = position_model(order_book_id)
-                positions[order_book_id].apply_trade(trade)
-                # FIXME
-                positions[order_book_id]._last_price = price
+            price = bars[0]
+            trade = _fake_trade(order_book_id, quantity, price)
+            if order_book_id not in positions:
+                positions[order_book_id] = position_model(order_book_id)
+            positions[order_book_id].apply_trade(trade)
+            # FIXME
+            positions[order_book_id]._last_price = price
 
         account = account_model(starting_cash, positions)
         units += account.total_value
