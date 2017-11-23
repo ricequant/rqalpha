76b2a66d56b940f34370da57d8232479d169b4be
Cedric Porter <cedricporter@gmail.com>
2017-11-24 00:30:16 +0800

    增量回测时增加日期检查避免重复跑

--- rqalpha/mod/rqalpha_mod_sys_incremental/mod.py
+++ rqalpha/mod/rqalpha_mod_sys_incremental/mod.py
@@ -22,6 +22,7 @@ from rqalpha.interface import AbstractMod
 from rqalpha.events import EVENT
 from rqalpha.const import PERSIST_MODE
 from rqalpha.utils.disk_persist_provider import DiskPersistProvider
+from rqalpha.utils.i18n import gettext as _
 
 from .recorder import CsvRecorder
 
@@ -36,6 +37,23 @@ class IncrementalMod(AbstractMod):
 
         env.config.base.persist = True
         env.config.base.persist_mode = PERSIST_MODE.ON_NORMAL_EXIT
+
+        self._meta_json_path = os.path.join(mod_config.persist_folder, "meta.json")
+        self._meta = {
+            "start_date": self._env.config.base.start_date.strftime("%Y-%m-%d"),
+            "end_date": self._env.config.base.end_date.strftime("%Y-%m-%d"),
+        }
+        if not os.path.exists(self._meta_json_path):
+            self._meta["origin_start_date"] = self._meta["start_date"]
+        else:
+            with open(self._meta_json_path, "r") as json_file:
+                persist_meta = json.load(json_file)
+            self._meta["origin_start_date"] = persist_meta["origin_start_date"]
+
+            if self._meta["start_date"] <= persist_meta["end_date"]:
+                raise RuntimeError(
+                    _(u"current start_date {} is before last end_date {}").format(self._meta["start_date"],
+                                                                                  persist_meta["end_date"]))
 
         if mod_config.use_disk_persist:
             persist_provider = DiskPersistProvider(os.path.join(mod_config.persist_folder, "persist"))
@@ -59,9 +77,11 @@ class IncrementalMod(AbstractMod):
         self._recorder.append_portfolio(calendar_dt, portfolio, bm_portfolio)
 
     def tear_down(self, success, exception=None):
-        if self._recorder:
-            if exception is None:
-                # 仅当成功运行才写入数据
-                self._recorder.flush()
-            self._recorder.close()
+        if exception is None:
+            # 仅当成功运行才写入数据
+            with open(self._meta_json_path, "w") as json_file:
+                json_file.write(json.dumps(self._meta))
 
+            if self._recorder:
+                self._recorder.flush()
+                self._recorder.close()
