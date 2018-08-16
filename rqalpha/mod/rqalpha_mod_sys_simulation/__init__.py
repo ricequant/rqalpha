df032aa971f24af71582aca2ff9d1f0ac372bf9a
amos365 <40923243+amos365@users.noreply.github.com>
2018-08-16 11:07:21 +0800

    Update __init__.py

--- rqalpha/mod/rqalpha_mod_sys_simulation/__init__.py
+++ rqalpha/mod/rqalpha_mod_sys_simulation/__init__.py
@@ -25,9 +25,9 @@ __config__ = {
     "matching_type": "current_bar",
     # 股票最小手续费
     "stock_min_commission": 0,
-    # 设置手续费乘数，默认为1
-    "commission_multiplier": 1,
-    # 设置印花税
+    # 基础佣金为10万分之一，设置手续费乘数，默认为30
+    "commission_multiplier": 30,
+    # 设置股票印花税
     "stock_tax_rate": 0.001,
     # price_limit: 在处于涨跌停时，无法买进/卖出，默认开启【在 Signal 模式下，不再禁止买进/卖出，如果开启，则给出警告提示。】
     "price_limit": True,
