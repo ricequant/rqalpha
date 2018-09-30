73d0195cf80bdc8f0d1f4fbc05a72b9f2ce418f5
supermaojj@qq.com <supermaojj@qq.com>
2018-09-30 15:27:17 +0800

    add ga

---
+++ rqalpha/backone/knapsack.py
@@ -0,0 +1,89 @@
+# coding: utf-8
+
+from pyeasyga import pyeasyga
+import datetime
+import pandas as pd
+import random
+from operator import attrgetter
+
+pd.set_option('display.height',1000)
+pd.set_option('display.max_rows',500)
+pd.set_option('display.max_columns',500)
+pd.set_option('display.width',1000)
+
+
+
+# setup data
+data = [{'name': 'chn.ln', 'cost': 3.15364, 'lower': 50, "avail": 2, 'province':"Ai", 'region':"ShangHai"},
+        {'name': 'chn.hh', 'cost': 2.80773, 'lower': 50, "avail": 2, 'province':"fff", 'region':"qq"},
+        {'name': 'chn.sx', 'cost': 3.15364, 'lower': 50, "avail": 2, 'province':"ZheJiang", 'region':"ShangHai"},
+        {'name': 'chn.sx', 'cost': 3.15364, 'lower': 50, "avail": 2, 'province':"ZheJiang", 'region':"ShangHai"},
+        {'name': 'chn.sx', 'cost': 3.15364, 'lower': 50, "avail": 2, 'province':"ZheJiang", 'region':"ShangHai"},
+        {'name': 'chn.sx', 'cost': 3.15364, 'lower': 60, "avail": 2, 'province':"ZheJiang", 'region':"ShangHai"},
+        {'name': 'chn.du', 'cost': 3.15364, 'lower': 50, "avail": 3, 'province':"ZheJiang", 'region':"ShangHai"}]
+
+today = datetime.date.today().strftime("%Y-%m-%d")
+today = "2018-09-29"
+filename = "top500/top500_%s" %  today
+df = pd.DataFrame.from_csv(filename)
+df = df.sort_values(by=['inc', 'rise_fall_ratio'], ascending=False)
+print df.head(500)
+data = df.head(500).to_dict(orient='records')
+
+
+def create_individual(data):
+    res =  [0 for _ in xrange(len(data))]
+    return res
+
+def crossover(parent_1, parent_2):
+    crossover_index = random.randrange(1, len(parent_1))
+    child_1 = parent_1[:crossover_index] + parent_2[crossover_index:]
+    child_2 = parent_2[:crossover_index] + parent_1[crossover_index:]
+    return child_1, child_2
+
+# define a fitness function
+def fitness(individual, data):
+    costs, rise_fall_region_ratio, avails, counts, province, region, lower, q= 0, 0, 0, 0, 1, 1, 1, 0
+    for selected, box in zip(individual, data):
+        if selected:
+            counts += 1
+            rise_fall_region_ratio += box.get('rise_fall_region_ratio')
+            
+                
+            q += rise_fall_region_ratio
+
+    if counts > ga.count:
+        q = 0
+    
+    return q
+
+def selection(population):
+    members = random.sample(population, 2)
+    members.sort(key=attrgetter('fitness'), reverse=True)
+    return members[0]
+
+
+def mutate(individual):
+    mutate_index = random.randrange(len(individual))
+    if individual[mutate_index] == 0:
+        individual[mutate_index] == 0
+    else:
+        individual[mutate_index] == 1
+
+
+if __name__ == "__main__":
+    ga = pyeasyga.GeneticAlgorithm(data)        # initialise the GA with data
+    ga.inc_ratio_limit = 0.5
+    ga.rise_all_ratio_limit = 0.7
+    ga.count = 50
+    ga.fitness_function = fitness               # set the GA's fitness function
+    ga.create_individual = create_individual
+    ga.crossover_function = crossover
+    #ga.mutate_function = mutate
+    #ga.selection_function = selection    
+    ga.run()
+    print ga.best_individual()
+    selected_list = ga.best_individual()[1]
+    for selected, s in zip(selected_list, data):
+        if selected:
+            print "%s %s %s %s %s" % (s["stock_id"], s["inc"], s["restore_predicted"], s["rise_fall"],   s["rise_fall_region"])
--- rqalpha/backone/merge_predicted_reslut.py
+++ rqalpha/backone/merge_predicted_reslut.py
@@ -3,7 +3,13 @@
 import pandas as pd
 import os
 import time
+import datetime
+from pyeasyga import pyeasyga
 
+pd.set_option('display.height',1000)
+pd.set_option('display.max_rows',500)
+pd.set_option('display.max_columns',500)
+pd.set_option('display.width',1000)
 
 def date_compare(item1, item2):
     t1 = time.mktime(time.strptime(item1, '%Y-%m-%d'))
@@ -35,31 +41,114 @@ def sort_files_by_date(base_dir):
         files_list.append(files_name)
     return files_list
     
+def sum_rise_fall(x, y):
+    return x+y
+
+def rise_fall_ok(x, count):
+    return float(x) / count
 
 def analyze_predicted_result(files):
     files_1 = files[:-1]
     files_2 = files[1:]
+    result =pd.DataFrame([])
+    count = 0
     for file1, file2 in zip(files_1, files_2):
         print file1
         print file2
-        calculate_index(file1, file2)
+        count = count + 1
         
+        df = calculate_index(file1, file2)
+        if not result.empty:
+            tmp_df = pd.merge(result, df, on='stock_id')
+            #print tmp_df
+            tmp_df["rise_fall"] = tmp_df.apply(lambda row: sum_rise_fall(row['rise_fall_x'], row['rise_fall_y']), axis=1)
+            tmp_df["rise_fall_region"] = tmp_df.apply(lambda row: sum_rise_fall(row['rise_fall_region_x'], row['rise_fall_region_y']), axis=1)
+            #print tmp_df["rise_fall_sum"]
+            result = tmp_df[["stock_id", "rise_fall", "rise_fall_region"]]
+        else:
+            result = df
+            #print df
+        #return []
         
-    return []
+    result["rise_fall_ratio"] = result.apply(lambda row: rise_fall_ok(row['rise_fall'], count), axis=1)
+    result["rise_fall_region_ratio"] = result.apply(lambda row: rise_fall_ok(row['rise_fall_region'], count), axis=1)
+    
+    
+    result = result.sort_values(by=['rise_fall_ratio'], ascending=False)
+    
+    today = datetime.date.today().strftime("%Y-%m-%d")
+    result.to_csv("rise_fall/rf_%s" %  today,  encoding = "utf-8")
+    return result
 
 def recommeder_socket(predicted_result_index):
-    return []
+    today = datetime.date.today().strftime("%Y-%m-%d")
+    to_day_predicted_reslut = "merge_predicted_reslut/merge_predicted_reslut%s.csv" % today
+    to_day_rise_fall = "rise_fall/rf_%s" %  today
+    today_df = pd.DataFrame.from_csv(to_day_predicted_reslut)
+    rise_fall_df = pd.DataFrame.from_csv(to_day_rise_fall)
+    
+    df = pd.merge(today_df, rise_fall_df, on='stock_id')
+    df = df.sort_values(by=['rise_fall_ratio'], ascending=False)
+    return df
 
-"""
-单只股票指标分析， 
-预测趋势正确率： 预测方向正确数 / 预测总天数  误差允许1%
-预测正确范围偏差： 真实值和预测值的差绝对值的方差
 
-全部股票总体：
-预测趋势正确率： 方向正确数 / 预测总数  误差允许1%
 
-"""
+def compare_rise_fall_region(a, b):
+    return abs(a-b)
+
+def compare_rise_fall(predicted, close_x, close_y):
+
+    a = close_x - close_y
+    b = predicted - close_y
+    a = int(a * 100)
+    b = int(b * 100)
+    if a > 0 and b > 0:
+        return 1
+    if a < 0 and b < 0:
+        return 1
+    if a == 0 and b ==  0:
+        return 1
+    return 0
+
 def calculate_index(file1, file2):
+    today_df = pd.DataFrame.from_csv(file1)
+    tomorrow_df = pd.DataFrame.from_csv(file2)
+    
+    df = pd.merge(today_df, tomorrow_df, on='stock_id')
+    #print df
+    
+    df['rise_fall'] = df.apply(lambda row: compare_rise_fall(row['restore_predicted_x'], row['yesterday_close_x'], row['yesterday_close_y']), axis=1)
+
+    df['rise_fall_region'] = df.apply(lambda row: compare_rise_fall_region(row['restore_predicted_x'], row['yesterday_close_y']), axis=1)
+    #print df[['restore_predicted_x', 'yesterday_close_x', 'yesterday_close_y', 'inc_x', 'rise_fall', 'rise_fall_region']]
+    a = df[df['rise_fall'] == 1]['rise_fall'].count()
+    b = df['rise_fall'].count()
+    oneday_predicted_rise_fall_is_ok =  a / float(b)
+    print oneday_predicted_rise_fall_is_ok
+    
+    
+    return df[["stock_id", "rise_fall", "rise_fall_region"]]
+    
+    left =  today_df[["stock_id","restore_predicted", "yesterday_close", "inc"]]
+    right =  tomorrow_df[["stock_id","yesterday_close"]]
+    
+    result = pd.merge(left, right, on='stock_id')
+    ddd =  result[["restore_predicted","yesterday_close_x","yesterday_close_y","inc"]]
+    
+    ddd["inc_check"] = ddd["yesterday_close_y"] - ddd["yesterday_close_x"] > 0
+    ddd["inc_check2"] =  ddd["inc"] > 0
+    ddd["result_1"] = ddd["inc_check2"] & ddd["inc_check"]
+    
+    
+    """
+    #print ddd[ddd["result_1"] == True].count() / ddd[ddd["inc_check"]== True].count()
+    print ddd[ddd["result_1"] == True].count() / ddd[ddd["inc_check2"]== True].count()
+    
+    print ddd[ddd["result_1"] == True].count()
+    print ddd[ddd["inc_check2"]== True].count()
+    """
+
+def calculate_index_test(file1, file2):
     today_df = pd.DataFrame.from_csv(file1)
     tomorrow_df = pd.DataFrame.from_csv(file2)
     
@@ -86,9 +175,12 @@ if __name__ == '__main__':
     print files
     
     predicted_result_index = analyze_predicted_result(files)
-    print predicted_result_index
+    print predicted_result_index.head(500)
     
-    top50 = recommeder_socket(predicted_result_index)
-    print top50
+    top500 = recommeder_socket(predicted_result_index)
+    top500 =  top500.head(500)
+    today = datetime.date.today().strftime("%Y-%m-%d")
+    top500.to_csv("top500/top500_%s" %  today,  encoding = "utf-8")
+    
     
     
