cc6faf1b964df94f2925750dc4d1e499e90947da
oscarcui <cuizi7@163.com>
2018-10-19 15:45:10 +0800

    fix typo

--- tests/unittest/__init__.py
+++ tests/unittest/__init__.py
@@ -4,12 +4,12 @@ from unittest import TestLoader, TestSuite, TextTestRunner
 
 
 def load_tests():
-    test_sute = TestSuite()
+    test_suite = TestSuite()
     this_dir = os.path.dirname(__file__)
     loader = TestLoader()
     package_tests = loader.discover(start_dir=this_dir)
-    test_sute.addTests(package_tests)
-    return test_sute
+    test_suite.addTests(package_tests)
+    return test_suite
 
 
 if __name__ == "__main__":
