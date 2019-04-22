5eb238ff6dee49f5cae3419fb4cf45230dea69c0
oscarcui <cuizi7@163.com>
2019-04-22 16:36:05 +0800

    be compatible with old bundle

--- rqalpha/data/share_transformation_store.py
+++ rqalpha/data/share_transformation_store.py
@@ -16,11 +16,57 @@
 
 import json
 
+from rqalpha.utils.logger import system_log
+
+DEFAULT_SHARE_TRANSFORMATION = {
+  "000024.XSHE": {
+    "successor": "001979.XSHE",
+    "effective_date": 1451433600000,
+    "share_conversion_ratio": 1.6008,
+    "predecessor_delisted": True,
+    "discretionary_execution": False,
+    "predecessor_delisted_date": 1451433600000,
+    "event": "merge"
+  },
+  "600005.XSHG": {
+    "successor": "600019.XSHG",
+    "effective_date": 1487030400000,
+    "share_conversion_ratio": 0.56,
+    "predecessor_delisted": True,
+    "discretionary_execution": False,
+    "predecessor_delisted_date": 1487030400000,
+    "event": "merge"
+  },
+  "601299.XSHG": {
+    "successor": "601766.XSHG",
+    "effective_date": 1432080000000,
+    "share_conversion_ratio": 1.1,
+    "predecessor_delisted": True,
+    "discretionary_execution": False,
+    "predecessor_delisted_date": 1432080000000,
+    "event": "merge"
+  },
+  "601313.XSHG": {
+    "successor": "601360.XSHG",
+    "effective_date": 1519603200000,
+    "share_conversion_ratio": 1.0,
+    "predecessor_delisted": True,
+    "discretionary_execution": False,
+    "predecessor_delisted_date": 1519603200000,
+    "event": "code_change"
+  }
+}
+
 
 class ShareTransformationStore(object):
     def __init__(self, f):
-        with open(f, 'r') as store:
-            self._share_transformation = json.load(store)
+        try:
+            with open(f, 'r', encoding="utf-8") as store:
+                self._share_transformation = json.load(store)
+        except FileNotFoundError:
+            # only for compatibility with the old bundle
+            system_log.warning("{} not found, use default data which may be out-of-date".format(f))
+            self._share_transformation = DEFAULT_SHARE_TRANSFORMATION
 
     def get_share_transformation(self):
         return self._share_transformation
