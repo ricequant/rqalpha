e338c34996b51665ecbff67b7688086aab67d89d
oscarcui <cuizi7@163.com>
2019-04-25 12:11:43 +0800

    refactor RealTimePriceSeriesBenchmarkProvider

--- rqalpha/mod/rqalpha_mod_sys_benchmark/benchmark_provider.py
+++ rqalpha/mod/rqalpha_mod_sys_benchmark/benchmark_provider.py
@@ -21,6 +21,7 @@ from rqalpha.interface import AbstractBenchmarkProvider
 from rqalpha.environment import Environment
 from rqalpha.events import EVENT
 from rqalpha.utils.i18n import gettext as _
+from rqalpha.utils.py2 import lru_cache
 
 
 class BackTestPriceSeriesBenchmarkProvider(AbstractBenchmarkProvider):
@@ -65,30 +66,41 @@ class RealTimePriceSeriesBenchmarkProvider(AbstractBenchmarkProvider):
     def __init__(self, order_book_id):
         self._order_book_id = order_book_id
         self._env = Environment.get_instance()
-
-        self._first_close = None
-
         self._daily_returns = 0
         self._total_returns = 0
-
-        self._env.event_bus.add_listener(EVENT.POST_SYSTEM_INIT, self._on_system_init)
         self._env.event_bus.prepend_listener(EVENT.BAR, self._on_bar)
 
-    def _on_system_init(self, _):
-        start_dt = datetime.combine(Environment.get_instance().config.base.start_date, time.min)
-        self._first_close = self._env.data_proxy.history_bars(
+    @property
+    @lru_cache()
+    def _first_close(self):
+        start_dt = datetime.combine(self._env.config.base.start_date, time.min)
+        return self._env.data_proxy.history_bars(
             self._order_book_id, 1, "1d", "close", start_dt, skip_suspended=False, adjust_type='pre'
         )[0]
 
+    def _update(self, bar=None):
+        if not (bar is None or bar.isnan):
+            self._daily_returns = float((bar.close - bar.prev_close) / bar.prev_close)
+            self._total_returns = float((bar.close - self._first_close) / self._first_close)
+        else:
+            close = self._env.data_proxy.history_bars(
+                self._order_book_id, 1, "1m", "close", self._env.calendar_dt, skip_suspended=False, adjust_type='pre'
+            )[0]
+            prev_close = self._env.data_proxy.get_prev_close(self._order_book_id, self._env.trading_dt)
+            self._daily_returns = float((close - prev_close) / prev_close)
+            self._total_returns = float((close - self._first_close) / self._first_close)
+
     def _on_bar(self, event):
-        bar = self._env.data_proxy.get_bar(self._order_book_id, event.calendar_dt, "1m")
-        self._daily_returns = float((bar.close - bar.prev_close) / bar.prev_close)
-        self._total_returns = float((bar.close - self._first_close) / self._first_close)
+        self._update(event.bar_dict[self._order_book_id])
 
     @property
     def daily_returns(self):
+        if not self._daily_returns:
+            self._update()
         return self._daily_returns
 
     @property
     def total_returns(self):
+        if not self._total_returns:
+            self._update()
         return self._total_returns
