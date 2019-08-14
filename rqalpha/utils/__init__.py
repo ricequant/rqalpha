6dcc96a41155aef53c25b6c36a6f0d583c1268bd
oscarcui <cuizi7@163.com>
2019-08-14 11:44:28 +0800

    remove unused utils

--- rqalpha/utils/__init__.py
+++ rqalpha/utils/__init__.py
@@ -32,21 +32,6 @@ from rqalpha.utils.i18n import gettext as _
 from rqalpha.utils.py2 import lru_cache
 
 
-def safe_round(value, ndigits=3):
-    if isinstance(value, float):
-        return round(value, ndigits)
-    return value
-
-
-class Singleton(type):
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-
-
 class RqAttrDict(object):
 
     def __init__(self, d=None):
