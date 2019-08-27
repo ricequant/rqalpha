a6af3e46062493432b8cb1b78a1ca417ef886feb
oscarcui <cuizi7@163.com>
2019-08-27 10:32:22 +0800

    remove patches for compatible

--- rqalpha/data/share_transformation_store.py
+++ rqalpha/data/share_transformation_store.py
@@ -17,74 +17,12 @@
 #     limitations under the License.
 
 import json
-import six
-
-from rqalpha.utils.logger import system_log
-
-DEFAULT_SHARE_TRANSFORMATION = {
-    '000022.XSHE': {
-        'successor': '001872.XSHE',
-        'effective_date': '2018-12-26',
-        'share_conversion_ratio': 1.0,
-        'predecessor_delisted': True,
-        'discretionary_execution': False,
-        'predecessor_delisted_date': '2018-12-26',
-        'event': 'code_change'
-    },
-    '000024.XSHE': {
-        'successor': '001979.XSHE',
-        'effective_date': '2015-12-30',
-        'share_conversion_ratio': 1.6008,
-        'predecessor_delisted': True,
-        'discretionary_execution': False,
-        'predecessor_delisted_date': '2015-12-30',
-        'event': 'merge'
-    },
-    '600005.XSHG': {
-        'successor': '600019.XSHG',
-        'effective_date': '2017-02-14',
-        'share_conversion_ratio': 0.56,
-        'predecessor_delisted': True,
-        'discretionary_execution': False,
-        'predecessor_delisted_date': '2017-02-14',
-        'event': 'merge'
-    },
-    '601299.XSHG': {
-        'successor': '601766.XSHG',
-        'effective_date': '2015-05-20',
-        'share_conversion_ratio': 1.1,
-        'predecessor_delisted': True,
-        'discretionary_execution': False,
-        'predecessor_delisted_date': '2015-05-20',
-        'event': 'merge'
-    },
-    '601313.XSHG': {
-        'successor': '601360.XSHG',
-        'effective_date': '2018-02-26',
-        'share_conversion_ratio': 1.0,
-        'predecessor_delisted': True,
-        'discretionary_execution': False,
-        'predecessor_delisted_date': '2018-02-26',
-        'event': 'code_change'
-    }
-}
-
-
-if six.PY2:
-    # FileNotFoundError is only available since Python 3.3
-    FileNotFoundError = IOError
-    from io import open
 
 
 class ShareTransformationStore(object):
     def __init__(self, f):
-        try:
-            with open(f, 'r', encoding="utf-8") as store:
-                self._share_transformation = json.load(store)
-        except FileNotFoundError:
-            # only for compatibility with the old bundle
-            system_log.warning("{} not found, use default data which may be out-of-date".format(f))
-            self._share_transformation = DEFAULT_SHARE_TRANSFORMATION
+        with open(f, 'r', encoding="utf-8") as store:
+            self._share_transformation = json.load(store)
 
     def get_share_transformation(self, order_book_id):
         try:
