156e85eeac9fbec00a579a4e96ceba0d9e2e3fe8
oscarcui <cuizi7@163.com>
2019-12-31 20:37:52 +0800

    fix PositionProxyDict

--- rqalpha/mod/rqalpha_mod_sys_accounts/account_model/position_proxy.py
+++ rqalpha/mod/rqalpha_mod_sys_accounts/account_model/position_proxy.py
@@ -14,7 +14,9 @@
 #         否则米筐科技有权追究相应的知识产权侵权责任。
 #         在此前提下，对本软件的使用同样需要遵守 Apache 2.0 许可，Apache 2.0 许可与本许可冲突之处，以本许可为准。
 #         详细的授权流程，请联系 public@ricequant.com 获取。
+import six
 from collections import UserDict
+from collections.abc import Mapping
 from typing import Dict, Type
 
 from rqalpha.environment import Environment
@@ -397,18 +399,33 @@ class PositionProxyDict(UserDict):
     def __init__(self, positions, position_proxy_cls):
         # type: (Dict[str, Dict[POSITION_DIRECTION, AssetPosition]], Type) -> PositionProxyDict
         super(PositionProxyDict, self).__init__()
-        self._cache = {}
         self._positions = positions
         self._position_proxy_cls = position_proxy_cls
 
-    def __missing__(self, order_book_id):
-        if order_book_id not in self._cache:
-            if order_book_id not in self._positions:
-                long = AssetPosition(order_book_id, POSITION_DIRECTION.LONG)
-                short = AssetPosition(order_book_id, POSITION_DIRECTION.SHORT)
-            else:
-                positions = self._positions[order_book_id]
-                long = positions[POSITION_DIRECTION.LONG]
-                short = positions[POSITION_DIRECTION.SHORT]
-            self._cache[order_book_id] = self._position_proxy_cls(long, short)
-        return self._cache[order_book_id]
+    def __getitem__(self, order_book_id):
+        if order_book_id not in self._positions:
+            long = AssetPosition(order_book_id, POSITION_DIRECTION.LONG)
+            short = AssetPosition(order_book_id, POSITION_DIRECTION.SHORT)
+        else:
+            positions = self._positions[order_book_id]
+            long = positions[POSITION_DIRECTION.LONG]
+            short = positions[POSITION_DIRECTION.SHORT]
+        return self._position_proxy_cls(long, short)
+
+    def keys(self):
+        return self._positions.keys()
+
+    def __contains__(self, item):
+        return item in self._positions
+
+    def __iter__(self):
+        return iter(self._positions)
+
+    def __len__(self):
+        return len(self._positions)
+
+    def __setitem__(self, key, value):
+        raise TypeError("{} object does not support item assignment".format(self.__class__.__name__))
+
+    def __delitem__(self, key):
+        raise TypeError("{} object does not support item deletion".format(self.__class__.__name__))
