8f0ce0389cc39fbe300cd328dcd43c88c4e93768
Hongzhang Liu <hongzhang.liu@gmail.com>
2020-02-15 15:59:52 +0800

    refactory: bundle_helper -> cmds/bundle

--- rqalpha/cmds/bundle.py
+++ rqalpha/cmds/bundle.py
@@ -13,9 +13,18 @@
 #         在此前提下，对本软件的使用同样需要遵守 Apache 2.0 许可，Apache 2.0 许可与本许可冲突之处，以本许可为准。
 #         详细的授权流程，请联系 public@ricequant.com 获取。
 import os
+import shutil
+import tarfile
+import tempfile
+import time
+import datetime
+import dateutil
 
-import six
 import click
+import requests
+import six
+
+from rqalpha.utils.i18n import gettext as _
 
 from .entry import cli
 
@@ -59,7 +68,70 @@ def update_bundle(data_bundle_path, compression):
 
 @cli.command()
 @click.option('-d', '--data-bundle-path', default=os.path.expanduser('~/.rqalpha'), type=click.Path(file_okay=False))
-def download_bundle(data_bundle_path):
+@click.option('--confirm', default=True, is_flag=True)
+def download_bundle(data_bundle_path, confirm):
     """download bundle (monthly updated)"""
-    import rqalpha.utils.bundle_helper
-    rqalpha.utils.bundle_helper.download_bundle(data_bundle_path)
+    default_bundle_path = os.path.abspath(os.path.expanduser('~/.rqalpha/bundle'))
+    if data_bundle_path is None:
+        data_bundle_path = default_bundle_path
+    else:
+        data_bundle_path = os.path.abspath(os.path.join(data_bundle_path, './bundle/'))
+    if (confirm and os.path.exists(data_bundle_path) and data_bundle_path != default_bundle_path and
+            os.listdir(data_bundle_path)):
+        click.confirm(_(u"""
+    [WARNING]
+    Target bundle path {data_bundle_path} is not empty.
+    The content of this folder will be REMOVED before updating.
+    Are you sure to continue?""").format(data_bundle_path=data_bundle_path), abort=True)
+
+    tmp = os.path.join(tempfile.gettempdir(), 'rq.bundle')
+    url, total_length = get_exactly_url()
+
+    with open(tmp, 'wb') as out:
+        download(out, total_length, url)
+
+    shutil.rmtree(data_bundle_path, ignore_errors=True)
+    os.makedirs(data_bundle_path)
+    tar = tarfile.open(tmp, 'r:bz2')
+    tar.extractall(data_bundle_path)
+    tar.close()
+    os.remove(tmp)
+    six.print_(_(u"Data bundle download successfully in {bundle_path}").format(bundle_path=data_bundle_path))
+
+
+CDN_URL = 'http://bundle.assets.ricequant.com/bundles_v4/rqbundle_%04d%02d.tar.bz2'
+
+
+def get_exactly_url():
+    day = datetime.date.today()
+    while True:  # get exactly url
+        url = CDN_URL % (day.year, day.month)
+        six.print_(_(u"try {} ...").format(url))
+        r = requests.get(url, stream=True)
+        if r.status_code == 200:
+            return url, int(r.headers.get('content-length'))
+
+        day -= dateutil.relativedelta.relativedelta(months=1)
+
+
+def download(out, total_length, url):
+    retry_interval = 3
+    retry_times = 5
+    with click.progressbar(length=total_length, label=_(u"downloading ...")) as bar:
+        for i in range(retry_times):
+            try:
+                headers = {'Range': "bytes={}-".format(bar.pos), }
+                r = requests.get(url, headers=headers, stream=True, timeout=10)
+                for data in r.iter_content(chunk_size=8192):
+                    bar.update(len(data))
+                    out.write(data)
+
+                if total_length == bar.pos:
+                    return True  # Download complete . exit
+            except requests.exceptions.RequestException:
+                if i < retry_times - 1:
+                    six.print_(_("\nDownload failed, retry in {} seconds.".format(retry_interval)))
+                    time.sleep(retry_interval)
+                else:
+                    raise
+
