7036ef71ec7118483b0883adf64e55a15226250e
崔子琦 <oscarcui@ricequant.com>
2020-02-27 19:48:02 +0800

    typo

--- rqalpha/data/bundle.py
+++ rqalpha/data/bundle.py
@@ -27,7 +27,7 @@ import rqdatac
 from rqalpha.utils.datetime_func import convert_date_to_date_int, convert_date_to_int
 
 START_DATE = 20050104
-END_DATE = 2999131
+END_DATE = 29991231
 
 
 def gen_instruments(d):
