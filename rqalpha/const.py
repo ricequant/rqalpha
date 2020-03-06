3d1c03ade7ca09e7a4fdd03d73473e91f9b9cb0b
zza <740713651@qq.com>
2020-03-06 10:46:11 +0800

    fix for python2.7

--- rqalpha/const.py
+++ rqalpha/const.py
@@ -32,7 +32,7 @@ class CustomEnumMeta(EnumMeta):
 
     def __getitem__(self, item):
         try:
-            return super().__getitem__(item)
+            return super(CustomEnumMeta, self).__getitem__(item)
         except KeyError:
             return self._member_reverse_map[item]
 
@@ -226,23 +226,23 @@ class FRONT_VALIDATOR_TYPE(CustomEnum):
 
 
 class CURRENCY(CustomEnum):
-    CNY = "CNY"     # 人民币
-    USD = "USD"     # 美元
-    EUR = "EUR"     # 欧元
-    HKD = "HKD"     # 港币
-    GBP = "GBP"     # 英镑
-    JPY = "JPY"     # 日元
-    KRW = "KWR"     # 韩元
-    CAD = "CAD"     # 加元
-    AUD = "AUD"     # 澳元
-    CHF = "CHF"     # 瑞郎
-    SGD = "SGD"     # 新加坡元
-    MYR = "MYR"     # 马拉西亚币
-    IDR = "IDR"     # 印尼币
-    NZD = "NZD"     # 新西兰币
-    VND = "VND"     # 越南盾
-    THB = "THB"     # 泰铢
-    PHP = "PHP"     # 菲律宾币
+    CNY = "CNY"  # 人民币
+    USD = "USD"  # 美元
+    EUR = "EUR"  # 欧元
+    HKD = "HKD"  # 港币
+    GBP = "GBP"  # 英镑
+    JPY = "JPY"  # 日元
+    KRW = "KWR"  # 韩元
+    CAD = "CAD"  # 加元
+    AUD = "AUD"  # 澳元
+    CHF = "CHF"  # 瑞郎
+    SGD = "SGD"  # 新加坡元
+    MYR = "MYR"  # 马拉西亚币
+    IDR = "IDR"  # 印尼币
+    NZD = "NZD"  # 新西兰币
+    VND = "VND"  # 越南盾
+    THB = "THB"  # 泰铢
+    PHP = "PHP"  # 菲律宾币
 
 
 UNDERLYING_SYMBOL_PATTERN = "([a-zA-Z]+)\d+"
