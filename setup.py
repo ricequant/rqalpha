990cf3b570fb274a89b92421352ca4ccd53fdd6a
cuizi7 <cuizi7@163.com>
2020-03-29 16:59:47 +0800

    .travis.yml & setup.py

--- setup.py
+++ setup.py
@@ -74,8 +74,6 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
