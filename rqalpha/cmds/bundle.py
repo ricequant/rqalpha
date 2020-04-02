014a3a658582ee4ccd6c32573fbeb18f0491814a
崔子琦 <oscarcui@ricequant.com>
2020-04-02 19:40:19 +0800

    update msgs

--- rqalpha/cmds/bundle.py
+++ rqalpha/cmds/bundle.py
@@ -67,18 +67,21 @@ def update_bundle(data_bundle_path, rqdatac_uri, compression, concurrency):
     try:
         import rqdatac
     except ImportError:
-        click.echo('rqdatac is required to create bundle')
+        click.echo(_(
+            'rqdatac is required to update bundle, '
+            'you can user "rqalpha download-bundle" to download to download monthly updated bundle'
+        ))
         return 1
 
     try:
         init_rqdatac_env(rqdatac_uri)
         rqdatac.init()
     except ValueError as e:
-        click.echo('rqdatac init failed with error: {}'.format(e))
+        click.echo(_('rqdatac init failed with error: {}').format(e))
         return 1
 
     if not os.path.exists(os.path.join(data_bundle_path, 'bundle')):
-        click.echo('bundle not exist, use create-bundle command instead')
+        click.echo(_('bundle not exist, use create-bundle command instead'))
         return 1
 
     from rqalpha.data.bundle import update_bundle as update_bundle_
