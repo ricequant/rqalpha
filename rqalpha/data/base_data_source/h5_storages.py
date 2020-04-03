e81b2e93c9a8855cecd3a1bf5ab0d49a7b5086b4
崔子琦 <oscarcui@ricequant.com>
2020-04-03 16:47:32 +0800

    handle non-ascii path on windows

--- rqalpha/data/base_data_source/h5_storages.py
+++ rqalpha/data/base_data_source/h5_storages.py
@@ -12,13 +12,14 @@
 #         未经米筐科技授权，任何个人不得出于任何商业目的使用本软件（包括但不限于向第三方提供、销售、出租、出借、转让本软件、本软件的衍生产品、引用或借鉴了本软件功能或源代码的产品或服务），任何法人或其他组织不得出于任何目的使用本软件，否则米筐科技有权追究相应的知识产权侵权责任。
 #         在此前提下，对本软件的使用同样需要遵守 Apache 2.0 许可，Apache 2.0 许可与本许可冲突之处，以本许可为准。
 #         详细的授权流程，请联系 public@ricequant.com 获取。
-import h5py
+
 import numpy as np
 import pandas as pd
 
 from rqalpha.utils.datetime_func import convert_date_to_date_int
 
 from .storages import AbstractDayBarStore
+from .date_set import open_h5
 
 
 DEFAULT_DTYPE = np.dtype([
@@ -33,7 +34,7 @@ DEFAULT_DTYPE = np.dtype([
 
 class DayBarStore(AbstractDayBarStore):
     def __init__(self, path):
-        self._h5 = h5py.File(path, 'r')
+        self._h5 = open_h5(path, mode="r")
 
     def get_bars(self, order_book_id):
         try:
@@ -51,7 +52,7 @@ class DayBarStore(AbstractDayBarStore):
 
 class DividendStore:
     def __init__(self, path):
-        self._h5 = h5py.File(path, 'r')
+        self._h5 = open_h5(path, mode="r")
 
     def get_dividend(self, order_book_id):
         try:
@@ -62,8 +63,7 @@ class DividendStore:
 
 class YieldCurveStore:
     def __init__(self, path):
-        with h5py.File(path, 'r') as h5:
-            self._data = h5['data'][:]
+        self._data = open_h5(path, mode="r")["data"][:]
 
     def get_yield_curve(self, start_date, end_date, tenor):
         d1 = convert_date_to_date_int(start_date)
@@ -91,7 +91,7 @@ class YieldCurveStore:
 
 class SimpleFactorStore:
     def __init__(self, path):
-        self._h5 = h5py.File(path, 'r')
+        self._h5 = open_h5(path, mode="r")
 
     def get_factors(self, order_book_id):
         try:
