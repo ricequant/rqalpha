0f3ecd23361eb5e79796f36112f717e0d8c85ab8
mar-heaven <775650117@qq.com>
2020-05-14 14:32:09 +0800

    remove report.xls

--- rqalpha/mod/rqalpha_mod_sys_analyser/report.py
+++ rqalpha/mod/rqalpha_mod_sys_analyser/report.py
@@ -26,14 +26,10 @@ def generate_report(result_dict, output_path):
     except:
         pass
 
-    xlsx_writer = pd.ExcelWriter(os.path.join(output_path, "report.xls"))
-
     # summary.csv
     csv_txt = StringIO()
     summary = result_dict["summary"]
     csv_txt.write(u"\n".join(sorted("{},{}".format(key, value) for key, value in six.iteritems(summary))))
-    df = pd.DataFrame(data=[{"val": val} for val in summary.values()], index=summary.keys()).sort_index()
-    df.to_excel(xlsx_writer, sheet_name="summary")
 
     with open(os.path.join(output_path, "summary.csv"), 'w') as csvfile:
         csvfile.write(csv_txt.getvalue())
@@ -54,10 +50,5 @@ def generate_report(result_dict, output_path):
         csv_txt = StringIO()
         csv_txt.write(df.to_csv(encoding='utf-8'))
 
-        df.to_excel(xlsx_writer, sheet_name=name)
-
         with open(os.path.join(output_path, "{}.csv".format(name)), 'w') as csvfile:
             csvfile.write(csv_txt.getvalue())
-
-    # report.xls <--- 所有sheet的汇总
-    xlsx_writer.save()
