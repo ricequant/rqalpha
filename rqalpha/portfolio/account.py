840b34f2d4eaf3e0c8a2fc047380f4fc7dbcf22c
zza <740713651@qq.com>
2020-09-09 18:12:50 +0800

    update persist logic

--- rqalpha/portfolio/account.py
+++ rqalpha/portfolio/account.py
@@ -15,22 +15,20 @@
 #         在此前提下，对本软件的使用同样需要遵守 Apache 2.0 许可，Apache 2.0 许可与本许可冲突之处，以本许可为准。
 #         详细的授权流程，请联系 public@ricequant.com 获取。
 
-import six
-from datetime import datetime
-from rqalpha.utils.functools import lru_cache
 from itertools import chain
-from typing import Dict, Iterable, Union, Optional, Type, Callable, List
+from typing import Callable, Dict, Iterable, List, Optional, Type, Union
 
-from rqalpha.const import POSITION_EFFECT
+import six
+
+from rqalpha.const import POSITION_DIRECTION, POSITION_EFFECT
 from rqalpha.core.events import EVENT
 from rqalpha.environment import Environment
-from rqalpha.const import POSITION_DIRECTION, INSTRUMENT_TYPE
-from rqalpha.utils.class_helper import deprecated_property
-from rqalpha.model.order import OrderStyle, Order
+from rqalpha.model.order import Order, OrderStyle
 from rqalpha.model.trade import Trade
-from rqalpha.utils.logger import user_system_log
+from rqalpha.utils.class_helper import deprecated_property
+from rqalpha.utils.functools import lru_cache
 from rqalpha.utils.i18n import gettext as _
-
+from rqalpha.utils.logger import user_system_log
 from .position import Position, PositionProxyDict
 
 OrderApiType = Callable[[str, Union[int, float], OrderStyle, bool], List[Order]]
@@ -113,7 +111,10 @@ class Account:
         for order_book_id, positions_state in state['positions'].items():
             for direction in POSITION_DIRECTION:
                 position = self._get_or_create_pos(order_book_id, direction)
-                position.set_state(positions_state.get(direction, positions_state[direction.lower()]))
+                if direction in positions_state.keys():
+                    position.set_state(positions_state[direction])
+                else:
+                    position.set_state(positions_state[direction.lower()])
 
     def fast_forward(self, orders=None, trades=None):
         if trades:
