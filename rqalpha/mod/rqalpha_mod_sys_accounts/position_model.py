4f94546e78a023f7b7b83509afdcf3b66726726b
mar-heaven <775650117@qq.com>
2021-08-05 17:29:26 +0800

    :bug: fix dividend_receivable

--- rqalpha/mod/rqalpha_mod_sys_accounts/position_model.py
+++ rqalpha/mod/rqalpha_mod_sys_accounts/position_model.py
@@ -186,10 +186,14 @@ class StockPosition(Position):
         self._dividend_receivable = None
         if self.dividend_reinvestment:
             last_price = self.last_price
-            self.apply_trade(Trade.__from_create__(
-                None, last_price, dividend_value / last_price, SIDE.BUY, POSITION_EFFECT.OPEN, self._order_book_id
-            ))
-            return 0
+            amount = int(Decimal(dividend_value) / Decimal(last_price))
+            round_lot = self._instrument.round_lot
+            amount = int(Decimal(amount) / Decimal(round_lot)) * round_lot
+            if amount > 0:
+                self.apply_trade(Trade.__from_create__(
+                    None, last_price, amount, SIDE.BUY, POSITION_EFFECT.OPEN, self._order_book_id
+                ))
+            return dividend_value - amount * last_price
         else:
             return dividend_value
 
