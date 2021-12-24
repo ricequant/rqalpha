3b94f51a21914c363131d01ec73bb2b7e81ea95e
Ginta <775650117@qq.com>
2021-12-24 17:46:01 +0800

    support 3.10

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     python_requires=">=3.6"
 )
