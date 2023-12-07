009514e917c1448b2c66709d093f3b629220e4bd
lin.dongzhao <542698096@qq.com>
2023-12-07 16:01:24 +0800

    数据更新

--- tests/api_tests/mod/sys_simulation/test_simulation_event_source.py
+++ tests/api_tests/mod/sys_simulation/test_simulation_event_source.py
@@ -18,8 +18,8 @@
 
 __config__ = {
     "base": {
-        "start_date": "2015-04-14",
-        "end_date": "2015-04-24",
+        "start_date": "2015-04-10",
+        "end_date": "2015-04-20",
         "frequency": "1d",
         "accounts": {
             "stock": 1000000,
