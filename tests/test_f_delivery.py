9bb89e11c56f4f293b0a81f82733bb09737c0062
lin.dongzhao <542698096@qq.com>
2024-03-21 17:13:03 +0800

    测试用例新增期货交割后的仓位、总权益验证

--- tests/test_f_delivery.py
+++ tests/test_f_delivery.py
@@ -1,3 +1,5 @@
+import datetime
+
 def init(context):
     context.f1 = "IH2402"
     context.f2 = "IC2402"
@@ -9,6 +11,14 @@ def handle_bar(context, bar_dict):
         buy_open(context.f1, 3)
         sell_open(context.f2, 3)
         context.fired = True
+    if bar_dict._dt.date() == datetime.date(2024, 2, 19):
+        context.cash_before_delivery = context.portfolio.cash
+        context.daily_pnl = context.portfolio.daily_pnl
+    if bar_dict._dt.date() == datetime.date(2024, 2, 20):
+        assert get_position(context.f1).quantity == 0
+        assert get_position(context.f2).quantity == 0
+        assert abs(context.portfolio.cash - (((2363 * 300) + (5105.6 * 200)) * 0.12 * 3 + context.cash_before_delivery + context.daily_pnl)) < 0.0000001
+        assert context.portfolio.total_value == context.portfolio.cash
 
 
 __config__ = {
