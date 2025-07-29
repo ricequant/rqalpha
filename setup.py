f72ca99ccb8953e5b9d95bf4d5cc9ff2e1d4f9be
Don <lin.dongzhao@ricequant.com>
2025-07-29 11:57:43 +0800

    revise reits trading

--- setup.py
+++ setup.py
@@ -84,6 +84,7 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
     python_requires=">=3.6"
 )
