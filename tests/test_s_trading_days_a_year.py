5fb98d81ca26858e4835d0faab60a00ae5a6ce18
Jeffrey <wangjiangfeng@hcyjs.com>
2025-07-31 16:38:21 +0800

    新增关于年化收益率计算的测试用例

--- tests/test_s_trading_days_a_year.py
+++ tests/test_s_trading_days_a_year.py
@@ -1,4 +1,6 @@
 from rqalpha.environment import Environment
+from rqalpha.const import DAYS_CNT
+
 
 def init(context):
     context.s1 = '600519.XSHG'
@@ -13,6 +15,20 @@ def handle_bar(context, bar_dict):
         # order_percent并且传入1代表买入该股票并且使其占有投资组合的100%
         order_percent(context.s1, 1)
         context.fired = True
+
+def after_trading(context):
+    logger.info(context.portfolio)
+    env = Environment.get_instance()
+    
+    # 计算按252天的年化收益率
+    date_count = float(env.data_proxy.count_trading_dates(context.config.base.start_date, env.trading_dt.date()))
+    unit_net_value = context.portfolio.unit_net_value
+    annualized_returns252 = unit_net_value ** (DAYS_CNT.TRADING_DAYS_A_YEAR / date_count) - 1
+    # 实际按242天的年化收益率
+    annualized_returns242 = context.portfolio.annualized_returns
+    annualized_returns = context.portfolio.annualized_returns
+    assert annualized_returns252 != annualized_returns242
+    assert annualized_returns242 == annualized_returns
 
 
 __config__ = {
